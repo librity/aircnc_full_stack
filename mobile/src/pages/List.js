9f6a04cd31268fff78b195952d594e8e2a716b5e
Librity <luis.geniole@gmail.com>
2019-10-14 10:50:24 -0300

    Finish second-to-last video & add socket.io

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
-import { View, Text, AsyncStorage } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  Image,
+  AsyncStorage
+} from "react-native";
+
+import SpotList from "../components/SpotList";
+
+import logo from "../assets/logo.png";
 
 export default function List() {
   const [techs, setTechs] = useState([]);
 
+  
   useEffect(() => {
     AsyncStorage.getItem("techs").then(storedTechs => {
       const techsArray = storedTechs.split(",").map(tech => tech.trim());
@@ -11,5 +23,28 @@ export default function List() {
     });
   }, []);
 
-  return <Text>{techs}</Text>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+
+      <ScrollView>
+        {techs.map(tech => (
+          <SpotList key={tech} tech={tech} />
+        ))}
+      </ScrollView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+
+  logo: {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 40
+  }
+});
